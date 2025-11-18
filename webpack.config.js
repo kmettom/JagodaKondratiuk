1190d81429eada6938c97934a69b7b15c7e0d52d
kmettom <kmettom@gmail.com>
2025-11-18 19:33:14 +0000

    :art: moiizo project init

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 // const VueLoaderPlugin = require('vue-loader/lib/plugin');
 // const ConcatPlugin = require('webpack-concat-plugin');
@@ -13,71 +13,72 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: './src/jk_portfolio.js',
-  output: {
-    // filename: 'jk_portfolio.js',
-    filename: process.env.NODE_ENV === 'production' ?  "jk_portfolio.[hash].js" : "jk_portfolio.js",
-    path: path.resolve(__dirname, 'dist'),
+    entry: './src/jk_portfolio.js',
+    output: {
+        // filename: 'jk_portfolio.js',
+        filename: process.env.NODE_ENV === 'production' ? "jk_portfolio.[hash].js" : "jk_portfolio.js",
+        path: path.resolve(__dirname, 'dist'),
 
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'src'),
-    compress: false,
-    port: 4200,
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              // data: `@import "./src/scss/portfolio.scss";`
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: false,
+        port: 4200,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            // data: `@import "./src/scss/portfolio.scss";`
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
             }
-          },
+            //  {
+            //   test: /\.html$/i,
+            //   loader: 'html-loader',
+            // },
+
         ],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]'
-        }
-      }
-     //  {
-     //   test: /\.html$/i,
-     //   loader: 'html-loader',
-     // },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: "index.html",
+            title: 'Caching',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/projects/moiizo.html',
+            filename: "projects/moiizo.html",
+            title: 'Caching',
+        }),
 
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: "index.html",
-      title: 'Caching',
-    }),
-
-    // new WebpackMd5Hash(),
-
-  // new HtmlWebpackPlugin()
-]
-
+    ]
 
 
 }
